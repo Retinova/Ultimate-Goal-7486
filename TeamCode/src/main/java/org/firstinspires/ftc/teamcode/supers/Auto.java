ff6d88c4f5acb138099c5d1f351a7068b9d56262
LoganL111 <77074597+LoganL111@users.noreply.github.com>
2021-03-17 15:59:20 -0400

    I tried to start to automate the claw rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/supers/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/supers/Auto.java
@@ -3,7 +3,20 @@ package org.firstinspires.ftc.teamcode.supers;
 public class Auto {
     private Robot r;
 
+    private boolean clawPos = false;
+
     public Auto(Robot robot){
         this.r = robot;
+    }
+
+    robot = new Robot(this, Mode.Auto);
+    waitForStart();
+
+    while(isStarted() && !isStopRequested()){
+        robot.clawRot.setTargetPosition(robot.clawRot.getCurrentPosition() + 144);
+
+        robot.clawRot.setPower(1.0);
+        clawPos = !clawPos;
+
     }
 }
